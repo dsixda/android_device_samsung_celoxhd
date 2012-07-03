513629c4da866db5434df448468394e2e0e9a785
dsixda <dislam@gmail.com>
2012-07-03 00:50:28 -0400

    Add enhanced NFC

--- cm.mk
+++ cm.mk
@@ -3,6 +3,9 @@ $(call inherit-product, device/samsung/celoxhd/full_celoxhd.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
