5e00234d1429e685159e613f90c471b537dce21b
dsixda <dislam@gmail.com>
2012-08-29 10:47:41 -0400

    Updated build fingerprint to GLH2

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I757M TARGET_DEVICE=SGH-I757M BUILD_FINGERPRINT="samsung/SGH-I757M/SGH-I757M:4.0.4/IMM76D/UCLE2:user/release-keys" PRIVATE_BUILD_DESC="SGH-I757M-user 4.0.4 IMM76D UCLE2 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I757M TARGET_DEVICE=SGH-I757M BUILD_FINGERPRINT="samsung/SGH-I757M/SGH-I757M:4.0.4/IMM76D/GLH2:user/release-keys" PRIVATE_BUILD_DESC="SGH-I757M-user 4.0.4 IMM76D GLH2 release-keys"
 
 TARGET_BOOTANIMATION_NAME := vertical-720x1280
 
